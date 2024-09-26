de0bb2c77ff590b38fb920f7b8d5f1de1c286743
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-09-26 18:24:44 +0700

    1 commit - выполнение домашки

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getNameAuthor() {
+        return this.name;
+    }
+
+    public String getSurnameAuthor() {
+        return this.surname;
+    }
+}
