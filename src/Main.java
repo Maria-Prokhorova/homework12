17d55929b9b6e16a9d11918237988d1a036808d5
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-09-29 11:30:26 +0700

    2 commit - выполнение второй домашки по классам и методам

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        Создайте класс Book, который будет содержать в себе данные о названии, авторе и годе публикации книги.
-        Убедитесь, что типы полей класса Book выбраны правильно: это String, Author и int.
-        Создайте класс Author, который содержит в себе данные об имени и фамилии автора.
-        Напишите конструкторы для обоих классов, заполняющие все поля.
-        Создайте геттеры для всех полей автора и всех полей книги.
-        Создайте сеттер для поля «Год публикации» у книги.
-        В методе main создайте несколько объектов «Книга» (достаточно двух) и несколько объектов «Автор» (достаточно
-        тоже двух) и инициализируйте их. Учтите, что авторы являются обязательными и книги не могут создаваться без
-        авторов.
-        Метод main не должен находиться в классах Book и Author.
-        Создайте отдельный класс для запуска приложения и объявите метод main в нем.
-        В том же методе main измените год публикации одной из книг с помощью сеттера.
+        Задание
+        Реализуйте методы toString, equals и hashCode в классах Author и Book, которые вы создали на прошлом уроке.
+        Обратите внимание, что toString книги не должен дублировать код из toString автора, а должен делегировать
+        (вызывать) его версию метода.
          */
 
         Author author1 = new Author("Дмитрий", "Глуховский");
         Author author2 = new Author("Айн", "Рэнд");
         Book book1 = new Book("Метро 2033", author1, 2007);
         Book book2 = new Book("Атлант расправил плечи", author2, 1957);
+        Book book3 = new Book("Метро 2033", author1, 2015);
+
 
         System.out.println("Название первой книги - " + book1.getTitleBook());
         System.out.println("Автор первой книги - " + book1.getAuthorBook().getNameAuthor() + " " + book1.getAuthorBook().getSurnameAuthor());
@@ -31,5 +25,13 @@ public class Main {
         book2.setYearPublication(2004);
         System.out.println("Год публикации второй книги после изменений - " + book2.getYearPublicationBook());
 
+        System.out.println();
+        System.out.println(book1);
+        System.out.println(book2);
+
+        System.out.println();
+        if (book1.equals(book3)) {
+            System.out.println("Это одна и та же книга, только имеет разный год публикации");
+        } else System.out.println("Это разные книги");
     }
 }
